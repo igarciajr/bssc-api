2e50bf51e00e748236db9afda6cd8db6f79d28c1
Isagani Garcia <igarcia@obsglobal.com>
2023-10-05 14:22:53 -0500

    Remove keys

--- TelesignIntegration/TelesignIntegration/TelesignIntegration/Telesign.cs
+++ TelesignIntegration/TelesignIntegration/TelesignIntegration/Telesign.cs
@@ -4,8 +4,8 @@ namespace TelesignIntegration
 {
     public class Telesign
     {
-        public static string _customerId = "E6457A41-9A57-4340-8C27-AB5428F29068";
-        public static string _apiKey = "bI8t/Q36QJmI1QMPnGwD01CX+6tinPR3hQOR0V/zpusk/bCVdbJUhxWFr5mhQbB2WLV9L7Ht8EPa35Sw9M6lhw==";
+        public static string _customerId = "xxxxxxxxxxxxx";
+        public static string _apiKey = "xxxxxxxxxxxxxxxxxxxxxx";
 
         public int generateCode(int min, int max)
         {
